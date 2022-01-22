a12db9726d0256b16271227359468901d4bdd3d8
tanvir-ux <tanviralamshuvo@gmail.com>
2022-01-22 11:39:19 +0600

    Spread/Rest operator in javascript...

---
+++ spread_operator.js
@@ -0,0 +1,34 @@
+const arr = [5, 6, 8, 4, 9]
+Math.max(...arr)
+
+// is same as 
+Math.max.apply(null, arr)
+
+const obj1 = {
+    a: 'a from obj1',
+    b: 'b from obj1',
+    c: 'c from obj1',
+    d: {
+        e: 'e from obj1',
+        f: 'f from obj1'
+    },
+}
+const obj2 = {
+    b: 'b from obj2',
+    c: 'c from obj2',
+    d: {
+        g: 'g from obj2',
+        h: 'h from obj2'
+    },
+}
+console.log({...obj1, ...obj2})
+console.log(Object.assign({}, obj1, obj2))
+
+function add(first, ...rest) {
+    return rest.reduce((sum, next) => sum + next, first)
+}
+function add() {
+    const first = arguments[0]
+    const rest = Array.from(arguments).slice(1)
+    return rest.reduce((sum, next) => sum + next, first)
+}
