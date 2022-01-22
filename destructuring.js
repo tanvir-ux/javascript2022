146a03f1c80aeebd17d76bddf4443bab9d714ec0
tanvir-ux <tanviralamshuvo@gmail.com>
2022-01-22 10:50:13 +0600

    Extend destructuring

--- destructuring.js
+++ destructuring.js
@@ -16,3 +16,26 @@ function makeCalculation3(obj) {
     return Math.floor((x + d + z) / 3)
 }
 console.log(makeCalculation3({x:4, y: 200}))
+
+// Destructuring Assignment
+
+function nestedArrayAndObject() {
+    const info = {
+        title: 'Once Upon a Time',
+        protagonist: {
+            name: 'Emma Watson',
+            enemies: [
+                {name: 'Regina Mills', title: 'Evil Queen'},
+                {name: 'Cora Mills', title: 'Queen of Hearts'},
+                {name: 'Peter Pan', title: `The boy who wouldn't grow up`},
+                {name: 'Zelena', title: 'The Wicked Witch'}
+            ],
+        },
+    }
+    const title = info.title
+    const protagonistName = info.protagonist.name
+    const enemy = info.protagonist.enemies[3]
+    const enemyTitle = enemy.title
+    const enemyName = enemy.name
+    return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`
+}
