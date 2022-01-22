fdd8d297a92f5d9dfa21f1bc9d5e5083dc06a87e
tanvir-ux <tanviralamshuvo@gmail.com>
2022-01-22 09:58:17 +0600

    Arrow functions and shorthand property names

---
+++ arrow_functions.js
@@ -0,0 +1,6 @@
+const getFive = () => 5
+const addFive = a => a + 5
+const divide = (a, b) => a / b
+
+console.log(addFive(5))
+console.log(divide(30, 5))
---
+++ shorthand_property_names.js
@@ -0,0 +1,7 @@
+const a = 'hello'
+const b = 42
+const c = {d: [true, false]}
+console.log({a, b, c})
+
+// this is same as this:
+console.log({a: a, b: b, c: c})
